aeb7f22c651f9c36d9aea6e0e50fe914d37dc9a1
Benedikt Ziemons <benedikt.ziemons@cern.ch>
2021-06-22 10:09:56 +0200

    Testing: Remove NO_XDIST on databases

--- tools/pytest.sh
+++ tools/pytest.sh
@@ -34,22 +34,6 @@ export PYTHONPATH="$RUCIO_LIB"
 export PYTEST_DISABLE_PLUGIN_AUTOLOAD="True"
 
 NO_XDIST="${NO_XDIST:-False}"
-if [[ "${RDBMS:-}" == "sqlite" ]]; then
-  # no parallel tests on sqlite, because of random "sqlite3.OperationalError: database is locked"
-  echo "Disabling parallel testing for sqlite"
-  NO_XDIST="True"
-elif [[ "${RDBMS:-}" =~ mysql.* ]]; then
-  # no parallel tests on mysql, because of random "pymysql.err.OperationalError:
-  # (1213, 'Deadlock found when trying to get lock; try restarting transaction')"
-  echo "Disabling parallel testing for mysql"
-  NO_XDIST="True"
-elif [[ "${RDBMS:-}" == "oracle" ]]; then
-  # no parallel tests on oracle, because of random "cx_Oracle.DatabaseError:
-  # ORA-00060: deadlock detected while waiting for resource"
-  echo "Disabling parallel testing for oracle"
-  NO_XDIST="True"
-fi
-
 if [[ "$NO_XDIST" == "False" ]]; then
   NO_XDIST="$(python -c 'import xdist; print(False)' ||:)"
 fi
